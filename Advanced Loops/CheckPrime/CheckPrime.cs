9fe957b36a07a4271f7009a00739022cba7456b5
AneliaDoychinova <aneliya_doychinova@abv.bg>
2017-03-31 11:37:39 +0300

    Added Check Prime

---
+++ Advanced Loops/CheckPrime/CheckPrime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPrime
+{
+    class CheckPrime
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+
+            var isPrime = true;
+
+            if (n < 2)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Pow(n, 0.5); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+
+                    }
+
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+
+             
+           
+        }
+    }
+}
