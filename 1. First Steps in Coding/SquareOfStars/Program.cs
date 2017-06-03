29f36d56fca0a1742f8ef3bc18b4436bacaf07db
AneliaDoychinova <aneliya_doychinova@abv.bg>
2017-06-03 14:59:18 +0300

    Added First steps in coding

---
+++ 1. First Steps in Coding/SquareOfStars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*',n));
+            for (int i = 0; i < n-2; i++)
+            {
+                Console.WriteLine("*" + new string (' ', n-2) + "*"); 
+            }
+
+            Console.WriteLine(new string('*', n));
+        }
+    }
+}
